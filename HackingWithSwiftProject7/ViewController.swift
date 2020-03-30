f0c83beeca684739a98995aef2d9b59703421e3d
Chen Sihan <schen@lcs.on.ca>
2020-03-30 15:09:19 -0400

    cellForRowAt, NumberOfRowsInSection, basic table view set up

--- HackingWithSwiftProject7/ViewController.swift
+++ HackingWithSwiftProject7/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class ViewController: UITableViewController {
     
-    
+    var petitions = [String]()
     
     
     
@@ -19,6 +19,21 @@ class ViewController: UITableViewController {
         // Do any additional setup after loading the view.
     }
 
+    override func tableView(_ tableView: UITableView,
+    numberOfRowsInSection section: Int) -> Int {
+       return petitions.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt
+    indexPath: IndexPath) -> UITableViewCell {
+       let cell = tableView.dequeueReusableCell(withIdentifier:
+    "Cell", for: indexPath)
+       
+       cell.textLabel?.text = "Title goes here"
+       // Subtitle of the cell
+       cell.detailTextLabel?.text = "Subtitle goes here"
+       return cell
+    }
 
 }
 
