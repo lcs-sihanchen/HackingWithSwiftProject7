97983cc45738c7f71dc472faf24e93fd14861975
Chen Sihan <schen@lcs.on.ca>
2020-04-01 12:32:58 -0400

    Have written all the code in the tutorial but the result is not expected, due to the differences in appDelegate.swift

--- HackingWithSwiftProject7/AppDelegate.swift
+++ HackingWithSwiftProject7/AppDelegate.swift
@@ -10,28 +10,45 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    
+    var window: UIWindow?
+    
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
         // Override point for customization after application launch.
+        // rootViewController is the initial controller, in this case it is supposed to be the ViewController, but it's embedded in a navigation controller that is embedded in a tab bar controller, therefore here it is a UITabBarController
+        if let tabBarController = window?.rootViewController as? UITabBarController {
+            // Since we need to create a new view controller, we need a reference to our storyboard
+            let storyboard = UIStoryboard(name: "Main", bundle: nil)
+            // Create the view controller wrapped in the navigation controller using this method
+            // We want the navigation controller to be passed in (Using the Storyboard ID we give before)
+            let vc = storyboard.instantiateViewController(withIdentifier:"NavController")
+            // Create a new tab bar item with toprated icon
+            // Use tag 1 for identification
+            vc.tabBarItem = UITabBarItem(tabBarSystemItem: .topRated, tag: 1)
+            // add the new view controller into the tab bar controller's view controller array
+            // Now there is one view controller in each tab
+            // The same code will be executed in this new view controller
+            tabBarController.viewControllers?.append(vc)
+        }
+        
         return true
     }
-
+    
     // MARK: UISceneSession Lifecycle
-
+    
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
-
+    
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
-
+    
+    
 }
 
--- HackingWithSwiftProject7/ViewController.swift
+++ HackingWithSwiftProject7/ViewController.swift
@@ -93,7 +93,14 @@ class ViewController: UITableViewController {
         // Download the content(JSON file)
         // let urlString = "https://api.whitehouse.gov/v1/petitions.json?limit=100"
         
-           let urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
+        let urlString: String
+        
+        if navigationController?.tabBarItem.tag == 0 {
+            urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
+        } else {
+            urlString = "https://www.hackingwithswift.com/samples/petitions-2.json"
+        }
+            
            // If let to make sure the URL is valid
            // Possibly more URLs here
            if let url = URL(string: urlString) {
@@ -102,9 +109,13 @@ class ViewController: UITableViewController {
               if let data = try? Data(contentsOf: url) {
                 // we're OK to parse!
                 parse(json: data)
+                return
                 }
         }
-                }
+        showError()
+        
+        
+        }
     
 
     override func tableView(_ tableView: UITableView,
@@ -151,6 +162,13 @@ class ViewController: UITableViewController {
        navigationController?.pushViewController(vc, animated: true)
     }
     
+    // Show the error if the webView does not load or if let statements doesn't pass
+    func showError() {
+       let ac = UIAlertController(title: "Loading error", message:
+    "There was a problem loading the feed; please check your connection and try again.", preferredStyle: .alert)
+       ac.addAction(UIAlertAction(title: "OK", style: .default))
+       present(ac, animated: true)
+    }
 
 }
 
